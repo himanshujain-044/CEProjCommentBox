f29c2efb6f71d24f51f3eec6e139b4b6e9d4bba2
Himanshu Jain <himanshujain@GS-5370.local>
2023-05-06 11:19:36 +0530

    feat: added comment box comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CommentBox from "./components/CommentBox/CommentBox";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const data = [
+    {
+      id: 3434,
+      comment: "This is sample comment. All paramters are okay for this order.",
+      dateAndTime: "08/05/2023 10:30:00",
+    },
+    {
+      id: 4454,
+      comment:
+        "This is sample comment. All paramters are okay for this order. Industry Dummy Text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliqua.",
+      dateAndTime: "08/05/2023 10:30:00",
+    },
+    {
+      id: 5252,
+      comment: "Please check your Estimated Dock Date for this order.",
+      dateAndTime: "08/05/2023 10:30:00",
+    },
+    {
+      id: 5252,
+      comment: "Please check your Estimated Dock Date for this order.",
+      dateAndTime: "08/05/2023 10:30:00",
+    },
+    {
+      id: 5252,
+      comment: "Please check your Estimated Dock Date for this order.",
+      dateAndTime: "08/05/2023 10:30:00",
+    },
+    {
+      id: 5252,
+      comment: "Please check your Estimated Dock Date for this order.",
+      dateAndTime: "08/05/2023 10:30:00",
+    },
+    {
+      id: 5252,
+      comment: "Please check your Estimated Dock Date for this order.",
+      dateAndTime: "08/05/2023 10:30:00",
+    },
+    {
+      id: 5252,
+      comment: "Please check your Estimated Dock Date for this order.",
+      dateAndTime: "09/05/2023 10:30:00",
+    },
+  ];
+
+  return <CommentBox data={data} />;
 }
 
 export default App;
---
+++ src/components/CommentBox/CommentBox.jsx
@@ -0,0 +1,100 @@
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import Popover from "@mui/material/Popover";
+import "./CommentBox.scss";
+
+const CommentSection = ({ text, dateTimePart }) => {
+  return (
+    <div className="cmnt-msg__cmnt-msg-text">
+      <p className="cmnt-msg__cmnt-msg-text--text">{text}</p>
+      <p className="cmnt-msg__cmnt-msg-text--date-time-wrapper">
+        <span className="cmnt-msg__cmnt-msg-text--date-and-time">
+          {dateTimePart.datePart}
+        </span>
+        <span className="cmnt-msg__cmnt-msg-text--date-and-time">
+          {dateTimePart.timePart}
+        </span>
+      </p>
+    </div>
+  );
+};
+const CommentBox = ({ data }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const id = open ? "comment-box-popover" : undefined;
+
+  const formatDateAndTime = (dateAndTime) => {
+    const [datePart, timePart] = dateAndTime.split(" ");
+    return { datePart, timePart };
+  };
+  if (!anchorEl) {
+    return (
+      <div className="pop-over">
+        <Button onClick={handleClick} variant="contained">
+          Open Comment Section
+        </Button>
+      </div>
+    );
+  }
+  return (
+    <Popover
+      id={id}
+      open={open}
+      anchorEl={anchorEl}
+      onClose={handleClose}
+      anchorPosition={{ top: 200, left: 440, position: "absolute" }}
+      anchorOrigin={{ vertical: "left", horizontal: "top" }}
+      transformOrigin={{}}
+    >
+      <div className="main-container">
+        <div className="heading">
+          <h3>Comment</h3>
+          <button className="heading__cross-icon" onClick={handleClose}>
+            X
+          </button>
+        </div>
+        <hr />
+        <div className="cmnt-section">
+          <h4>Add Comment</h4>
+          <textarea id="text-area" placeholder="start typing..." />
+          <div className="cmnt-section__action-btns">
+            <Button
+              variant="contained"
+              className="cmnt-section__action-btns--cancel-btn"
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              className="cmnt-section__action-btns--save-btn"
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+        <hr />
+        <div className="cmnt-msg">
+          <h4>Earlier Comments ({data.length ? data.length : 0})</h4>
+          <div className="cmnt-msg__comments-cont">
+            {data.length &&
+              data.map((item) => (
+                <CommentSection
+                  key={item.id}
+                  text={item.comment}
+                  dateTimePart={formatDateAndTime(item.dateAndTime)}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </Popover>
+  );
+};
+
+export default CommentBox;
